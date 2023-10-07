7ff6af4c43fbc4d3eb1bcde24497e9520815e0b3
igudy <goodnessigunma1@gmail.com>
2023-10-07 13:22:59 +0100

    basic redux increment and decrement

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementAsync } from "./slice/counterSlice";
 
 function App() {
-	return (
-		<div>
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
 
-		</div>
-	);
+  return (
+    <div className="text-center justify-center mt-72">
+      Redux Toolkit
+      <div className="block">
+        <button
+          className="border bg-slate-600 w-10 h-10 rounded-xl"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
+        <span className="mx-20">{count}</span>
+        <button
+          className="border bg-slate-600 w-10 h-10 rounded-xl"
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </button>
+
+        <div className="">
+          <input
+            type="text"
+            placeholder="Input Text"
+            value={incrementAmount}
+            className="w-10 h-10 mx-12 text-blue-500"
+            onChange={(e) => setIncrementAmount(e.target.value)}
+            border
+          />
+          <button
+            className="bg-gray-300 p-3 my-3 rounded-xl"
+            onClick={() =>
+              dispatch(incrementAsync(Number(incrementAmount) || 0))
+            }
+          >
+            Add Amount
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/slice/counterSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    increment: (state) => {
+      state.value = state.value + 1;
+    },
+    decrement: (state) => {
+      state.value = state.value - 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value = state.value + action.payload;
+    },
+  },
+});
+
+export const incrementAsync = (amount) => (dispatch) => {
+  setTimeout(() => {
+    dispatch(incrementByAmount(amount));
+  }, 1000);
+};
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state) => state.counter.value)`
+export const selectCount = (state) => state.counter.value;
+
+export default counterSlice.reducer;
